ae93a49b2ea95d7894443aa681ad2d7157247db7
hao1454507493@sina.com <hao873835932>
2020-02-06 12:42:36 +0800

    ff

--- ymlm.js
+++ ymlm.js
@@ -1,1 +1,25 @@
-d
+/*
+软件地址 http://jdytv.cn/app/index/qudao.html?uid=Mjk5NjE=
+[rewrite_local]
+#筋斗云
+^http:\/\/jdytv\.cn\/login\/login\/veifys\.html url script-response-body js/jindouyu1n.js
+[mitm]
+hostname = jdytv.cn
+ */
+
+
+
+//const path = "/api/public/";
+
+//const url = $request.url;
+var body = $response.body;
+
+
+var obj = JSON.parse(body);
+obj.msg.time = 1896192000;
+
+body = JSON.stringify(obj);
+
+
+$done({body});
+
